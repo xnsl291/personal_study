d5cce3f123cbe15fe763002df2603e6ca6ed3655
hana cho <sjfn242@gmail.com>
2023-04-09 18:54:19 +0900

    modify 주민등록번호생성: 월의 마지막 날짜 구하는 방식 변경.java

--- QUIZ1/JavaStudy04.java
+++ QUIZ1/JavaStudy04.java
@@ -1,10 +1,10 @@
 /*
-* 주제 : 주민등록번호 생성 프로그램
-* 요구사항 : 로직에 맞는 주민등록번호 생성
-* 생년월일(6자리)-성별(1자리)+랜덤번호(6자리)
-*
-* 작성자 : 조하나
-*/
+ * 주제 : 주민등록번호 생성 프로그램
+ * 요구사항 : 로직에 맞는 주민등록번호 생성
+ * 생년월일(6자리)-성별(1자리)+랜덤번호(6자리)
+ *
+ * 작성자 : 조하나
+ */
 
 import java.util.Scanner;
 import java.time.YearMonth;
@@ -53,12 +53,12 @@ public class JavaStudy04
         int month = get_input("출생월을 입력해주세요.(mm):",1,12);
 
         /*min : 1, max: 해당 년월의 마지막날*/
-        LocalDate end_day = YearMonth.from(LocalDate.parse(String.format("%s-%02d-01",year,month))).atEndOfMonth();
-        int day = get_input("출생일을 입력해주세요.(dd):",1,Integer.parseInt(end_day.toString().substring(8,10)));
+        int end_day = YearMonth.from(LocalDate.of(Integer.parseInt(year),month,1)).atEndOfMonth().getDayOfMonth();
+        int day = get_input("출생일을 입력해주세요.(dd):",1,end_day);
 
         int gender = get_input("성별을 입력해 주세요.(m/f):" , Integer.parseInt(year));
 
         /*yy+mm+dd - 성별+랜덤 6자리 숫자*/
-        System.out.printf("%d%02d%02d-%d%06d",Integer.parseInt(year.substring(2,4)),month,day,gender,rand.nextInt(1000000));
+        System.out.printf("%s%02d%02d-%d%06d",year.substring(2,4),month,day,gender,rand.nextInt(1000000));
     }
 }
