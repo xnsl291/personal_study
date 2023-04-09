96f281507e26efa3fdccd87adb4c05f74b6f07ff
hana cho <sjfn242@gmail.com>
2023-04-09 15:24:21 +0900

    modify 놀이동산 입장권 : 입력값 마다 값 검사하는 조건 추가.java

--- QUIZ1/JavaStudy03.java
+++ QUIZ1/JavaStudy03.java
@@ -1,13 +1,13 @@
 /*
-* 주제 : 놀이동산 입장권 계산 프로그램
-* 요구사항 : 입장료(기본 10000원)는 다음과 같은 우대조건에 따라 다름
-* - 나이, 입장시간, 국가유공자, 복지카드 여부순으로 입력
-* - 3세 미만 무료입장,
-* - 13세 미만 or 17시 이후입장 특별할인(4000),
-* - 복지카드, 국가유공자 일반 할인(8000) 적용
-*
-* 작성자 : 조하나
-*/
+ * 주제 : 놀이동산 입장권 계산 프로그램
+ * 요구사항 : 입장료(기본 10000원)는 다음과 같은 우대조건에 따라 다름
+ * - 나이, 입장시간, 국가유공자, 복지카드 여부순으로 입력
+ * - 3세 미만 무료입장,
+ * - 13세 미만 or 17시 이후입장 특별할인(4000),
+ * - 복지카드, 국가유공자 일반 할인(8000) 적용
+ *
+ * 작성자 : 조하나
+ */
 
 import java.util.Scanner;
 
@@ -19,22 +19,35 @@ public class JavaStudy03 {
 
     public static void main(String[] args)
     {
-        System.out.printf("[입장권 계산]\n나이를 입력해 주세요.(숫자):");
-        int age = scan.nextInt();
+        int age=0,entrance=0;
+        String nation_merit ="",welfare_merit="";
 
-        System.out.printf("입장시간을 입력해 주세요.(숫자입력):");
-        int entrance = scan.nextInt();
-        /*시간 범위 외 입력시 continue */
+        while(age<=0)
+        {
+            System.out.printf("[입장권 계산]\n나이를 입력해 주세요.(숫자):");
+            age = scan.nextInt();
+        }
 
-        System.out.printf("국가유공자 여부를 입력해 주세요.(y/n):");
-        String merit1 = scan.next();
+        while(entrance<1 || entrance > 24)
+        {
+            System.out.printf("입장시간을 입력해 주세요.(숫자입력):");
+            entrance = scan.nextInt();
+        }
 
-        System.out.printf("복지카드 여부를 입력해 주세요.(y/n):");
-        String merit2 = scan.next();
+        while(!nation_merit.equals("n")&& !nation_merit.equals("y"))
+        {
+            System.out.printf("국가유공자 여부를 입력해 주세요.(y/n):");
+            nation_merit = scan.next();
+        }
 
-        int fee = age<3 ? 0 : (age <13 || entrance>=17) ? SPECIAL_D : (merit1=="y"||merit1=="Y" || merit2=="y"||merit2=="Y") ? BASIC_D : ORIGINAL;
+        while(!welfare_merit.equals("n")&& !welfare_merit.equals("y"))
+        {
+            System.out.printf("복지카드 여부를 입력해 주세요.(y/n):");
+            welfare_merit = scan.next();
+        }
 
+        int fee = age<3 ? 0 : (age <13 || entrance>=17) ? SPECIAL_D : (nation_merit=="y" || welfare_merit=="y") ? BASIC_D : ORIGINAL;
         System.out.printf("입장료: %d",fee);
-
     }
+
 }
